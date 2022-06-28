ec21e0001dc9653665931036494d7194ede01462
Pol Orus <polorus84@gmail.com>
2022-06-28 23:11:59 +0200

    persistencia CSV DONE

--- src/Persistance/Fronts/Csv/CsvEditions.java
+++ src/Persistance/Fronts/Csv/CsvEditions.java
@@ -1,7 +1,12 @@
 package Persistance.Fronts.Csv;
 
 import business.Editions.Edition;
+import presentation.Menus.MenuMain;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -10,10 +15,76 @@ public class CsvEditions {
     public List<Edition> getEditions() {
         List<Edition> editionList = new LinkedList<>();
 
+        String path = "./files/csv/editions/editions.csv";
+
+        try {
+            String c = new String(Files.readAllBytes(Path.of(path)));
+            editionList = getList(c);
+        }catch (Exception e){
+            new MenuMain().showMessage("File not found");
+        }
+
         return editionList;
     }
 
-    public void writeEditions(List<Edition> editions) {
+    private List<Edition> getList(String c) {
+        List<Edition> editions = new LinkedList<>();
 
+        for (String s : c.split(System.lineSeparator())){
+            if (!s.isEmpty()){
+                int year = 0, playersNum = 0, trialsNum = 0, i=0;
+                List<String> trials = new LinkedList<>();
+
+                for (String camp : s.split(",")) {
+                    if (i < 3){
+                        switch (i){
+                            case 0 -> year = Integer.parseInt(camp);
+                            case 1 -> playersNum = Integer.parseInt(camp);
+                            case 2 -> trialsNum = Integer.parseInt(camp);
+                        }
+                    }else {
+                        trials.add(camp);
+                    }
+                    i++;
+                }
+                Edition e = new Edition(year, playersNum, trialsNum, trials);
+                editions.add(e);
+            }
+        }
+        return editions;
     }
+
+
+    public void writeEditions(List<Edition> editions) {
+        StringBuilder subject = new StringBuilder();
+
+        for (Edition e : editions) {
+            subject.append(this.csvConverter(e)).append(System.lineSeparator());
+        }
+        try {
+            FileWriter fileWriter = new FileWriter("./files/csv/editions/editions.csv");
+            fileWriter.write(subject.toString());
+            fileWriter.close();
+
+            new MenuMain().showMessage("File updated");
+        }catch (Exception e){
+            new MenuMain().showMessage("File not found");
+        }
+    }
+
+    private String csvConverter(Edition edition){
+        StringBuilder editionCsv = new StringBuilder();
+
+        editionCsv.append(edition.getYear());
+        editionCsv.append(",").append(edition.getPlayersQuantity());
+        editionCsv.append(",").append(edition.getTrials().size());
+
+        for (String trial : edition.getTrials()){
+            editionCsv.append(",").append(trial);
+        }
+
+        return editionCsv.toString();
+    }
+
+
 }
--- src/Persistance/Fronts/Csv/CsvTrials.java
+++ src/Persistance/Fronts/Csv/CsvTrials.java
@@ -1,25 +1,98 @@
 package Persistance.Fronts.Csv;
 
 import business.Trials.Trial;
-import business.Trials.Types.Types;
+import business.Trials.Types.*;
 import presentation.Menus.MenuMain;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.LinkedList;
 import java.util.List;
 
+                                //TODO : leer trials + pasarlas a CSV + escribirlas
 public class CsvTrials {
     public List<Trial> getTrials() {
         List<Trial> trialist = new LinkedList<>();
 
         for (Types t : Types.values()){
-            String path = "./files/csv/trials/\" + types.toString().toLowerCase() + \".csv";
+            String path = "./files/csv/trials/" + t.toString().toLowerCase() + ".csv";
 
             try{
                 String c = new String(Files.readAllBytes(Path.of(path)));
 
+                for (String row : c.split(System.lineSeparator())){
+                    if (!row.isEmpty()){
+                        switch (t){
+                            case paper_publication -> {
+                                int i = 1, acceptation = 0, revision = 0, refuse = 0;;
+                                String trialName = "", journalName = "", quartile = "";
+                                for (String camp : row.split(",")) {
+                                    switch (i) {
+                                        case 1 -> trialName = camp;
+                                        case 2 -> journalName = camp;
+                                        case 3 -> quartile = camp;
+                                        case 4 -> acceptation = Integer.parseInt(camp);
+                                        case 5 -> revision = Integer.parseInt(camp);
+                                        case 6 -> refuse = Integer.parseInt(camp);
+                                        default -> {}
+                                    }
+                                    i++;
+                                }
+                                trialist.add(new PaperPublication(trialName, journalName, quartile, acceptation, revision, refuse));
+                            }
+                            case master_studies -> {
+                                String trialName = "", masterName = "";
+                                int i = 1, ects = 0, probability = 0;
+
+                                for (String camp : row.split(",")) {
+                                    switch (i) {
+                                        case 1 -> trialName = camp;
+                                        case 2 -> masterName = camp;
+                                        case 3 -> ects = Integer.parseInt(camp);
+                                        case 4 -> probability = Integer.parseInt(camp);
+                                        default -> {}
+                                    }
+                                    i++;
+                                }
+                                trialist.add(new MasterStudies(trialName, masterName, ects, probability));
+
+                            }
+                            case doctoral_thesis -> {
+                                int i = 1, defenseDifficulty = 0;
+                                String trialName = "", studyField = "";
+
+                                for (String camp : row.split(",")) {
+                                    switch (i) {
+                                        case 1 -> trialName = camp;
+                                        case 2 -> studyField = camp;
+                                        case 3 -> defenseDifficulty = Integer.parseInt(camp);
+                                        default -> {}
+                                    }
+                                    i++;
+                                }
+                                trialist.add(new DoctoralThesis(trialName, studyField, defenseDifficulty));
+                            }
+                            case budget_request -> {
+                                int budget = 0, i = 1;
+                                String trialName = "", entityName = "";
+
+                                for (String camp : row.split(",")) {
+                                    switch (i) {
+                                        case 1 -> trialName = camp;
+                                        case 2 -> entityName = camp;
+                                        case 3 -> budget = Integer.parseInt(camp);
+                                        default -> {}
+                                    }
+                                    i++;
+                                }
+                                trialist.add(new BudgetRequest(trialName, entityName, budget));
+
+                            }
+                        }
+                    }
+                }
 
             } catch (IOException e) {
                 e.printStackTrace();
@@ -29,7 +102,52 @@ public class CsvTrials {
         return trialist;
     }
 
-    public void writeTrials(List<Trial> typeOfTrials, Types types) {
+    private String csvConverter(Trial trial, Types types){
+        StringBuilder trialCsv = new StringBuilder();
+        trialCsv.append(trial.getName());
 
+        switch (types){
+            case paper_publication -> {
+                trialCsv.append(",").append(((PaperPublication) trial).getJournalName());
+                trialCsv.append(",").append(((PaperPublication) trial).getQuartile());
+                trialCsv.append(",").append(((PaperPublication) trial).getAcceptationProbability());
+                trialCsv.append(",").append(((PaperPublication) trial).getRevisionProbability());
+                trialCsv.append(",").append(((PaperPublication) trial).getRefuseProbability());
+            }
+            case master_studies -> {
+                trialCsv.append(",").append(((MasterStudies) trial).getMasterName());
+                trialCsv.append(",").append(((MasterStudies) trial).getCreditsQuantity());
+                trialCsv.append(",").append(((MasterStudies) trial).getApproveCreditProbability());
+            }
+            case doctoral_thesis -> {
+                trialCsv.append(",").append(((DoctoralThesis) trial).getStudyField());
+                trialCsv.append(",").append(((DoctoralThesis) trial).getDefenseDifficulty());
+            }
+            case budget_request -> {
+                trialCsv.append(",").append(((BudgetRequest) trial).getEntityName());
+                trialCsv.append(",").append(((BudgetRequest) trial).getBudgetQuantity());
+            }
+        }
+        return trialCsv.toString();
+    }
+
+    public void writeTrials(List<Trial> trials, Types types) {
+        StringBuilder subject = new StringBuilder();
+
+        for(Trial t : trials) {
+            subject.append(this.csvConverter(t, types)).append(System.lineSeparator());
+        }
+
+        try {
+            FileWriter fileWriter = new FileWriter("./files/csv/trials/" + types.toString().toLowerCase() + ".csv");
+
+            fileWriter.write(subject.toString());
+            fileWriter.close();
+
+            new MenuMain().showMessage("File updated");
+
+        }catch (Exception e){
+            new MenuMain().showMessage("File not found");
+        }
     }
 }
